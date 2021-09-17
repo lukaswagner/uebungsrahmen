fd7f47d804b7c38630350c6152e149a795b1aa78
Lukas Wagner <lukas.wagner@hpi.de>
2021-09-17 13:30:32 +0200

    add fullscreen helper

--- exercises/webglOperateExample/code/index.ts
+++ exercises/webglOperateExample/code/index.ts
@@ -8,6 +8,7 @@ import {
 import { DemoRenderer } from './renderer';
 
 import { UI } from '@lukaswagner/web-ui';
+import { addFullscreenCheckbox, setupFullscreen } from 'fw/code/fullscreen';
 
 // for exposing canvas, controller, context, and renderer
 declare global {
@@ -21,13 +22,7 @@ declare global {
 
 const container = document.getElementById('content-container') as HTMLDivElement;
 
-container.addEventListener('dblclick', () => {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-    } else {
-        container.requestFullscreen();
-    }
-});
+setupFullscreen(container);
 
 const htmlCanvas = document.getElementById('content') as HTMLCanvasElement;
 const options: WebGLContextAttributes = {};
@@ -42,15 +37,21 @@ window.renderer = renderer;
 
 const controls = document.getElementById('controls') as HTMLDivElement;
 const ui = new UI(controls);
+
+addFullscreenCheckbox(container, ui);
+
 const setColor = (index: number, color: string) =>
     renderer.setColor(index, Color.hex2rgba(color).slice(0, 3));
+
 const color0 = ui.input.text({
     label: 'First color', type: 'color', value: '#aa0000',
     handler: setColor.bind(undefined, 0)
 });
 setColor(0, color0.value);
+
 const color1 = ui.input.text({
     label: 'Second color', type: 'color', value: '#0000aa',
     handler: setColor.bind(undefined, 1)
 });
 setColor(1, color1.value);
+color0.value = '#000000';
---
+++ source/code/fullscreen.ts
@@ -0,0 +1,28 @@
+import { UI } from "@lukaswagner/web-ui";
+
+export function setFullscreen(elem: HTMLElement, full: boolean = true) {
+    if (full) {
+        elem.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
+
+export function toggleFullscreen(elem: HTMLElement) {
+    setFullscreen(elem, !document.fullscreenElement);
+}
+
+export function setupFullscreen(elem: HTMLElement, event: string = 'dblclick') {
+    elem.addEventListener(event, () => toggleFullscreen(elem));
+}
+
+export function addFullscreenCheckbox(elem: HTMLElement, ui: UI) {
+    const checkbox = ui.input.checkbox({
+        label: 'Fullscreen',
+        handler: (v) => setFullscreen(elem, v)
+    });
+    document.addEventListener(
+        'fullscreenchange',
+        () => checkbox.value = !!document.fullscreenElement
+    );
+}
