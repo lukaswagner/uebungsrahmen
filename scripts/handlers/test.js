41734decb2c3dc06dd6bf56850fb5bf65eb5ea8a
Lukas Wagner <lukas@lwgnr.dev>
2021-10-11 16:01:23 +0200

    complete test script

--- scripts/handlers/test.js
+++ scripts/handlers/test.js
@@ -4,8 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const child = require('child_process');
+const stream = require('stream');
 const log = require('../helpers/log');
 const json = require('../helpers/json');
+const { stdout } = require('process');
+const kill = require('../helpers/kill');
 
 function heading(...text) {
     const mark = '=====';
@@ -18,7 +21,7 @@ function heading(...text) {
  * @param {child.SpawnSyncOptionsWithStringEncoding} options
  * @returns {child.SpawnSyncReturns<string>}
  */
-function run(cmd, args, options) {
+function runSync(cmd, args, options) {
     console.log(log.blue('Running command:'), cmd, ...args);
     const result = child.spawnSync(cmd, args, options);
     if (result.status !== 0) {
@@ -27,10 +30,30 @@ function run(cmd, args, options) {
     return result;
 }
 
-const test = (argv) => {
+/**
+ * @param {string} cmd
+ * @param {string[]} args
+ * @param {child.SpawnSyncOptionsWithStringEncoding} options
+ * @returns {child.ChildProcess}
+ */
+function run(cmd, args, options) {
+    console.log(log.blue('Running command:'), cmd, ...args);
+    return child.spawn(cmd, args, options);
+}
+
+function editFile(file, func, parseJson = false) {
+    const read = parseJson ? json.read : fs.readFileSync;
+    const write = parseJson ? json.write : fs.writeFileSync;
+    const content = read(file, { encoding: 'utf8' });
+    const edited = func(content);
+    write(file, edited, { encoding: 'utf8' });
+    return () => write(file, content, { encoding: 'utf8' });
+}
+
+async function test(argv) {
     const dir = './temp';
     const config = './temp.json';
-    const configArg = ['--config', config];
+    const configArgs = ['--config', config];
     const yesArg = '--assumeYes';
     const command = os.platform() === 'win32' ? '.\\fw.bat' : './fw.sh';
     const options = { stdio: 'inherit', shell: true, encoding: 'utf8' };
@@ -50,12 +73,12 @@ const test = (argv) => {
 
     // setup temp config and dir
     heading('Creating temporary assignment setup');
-    run(command, [
+    runSync(command, [
         'init',
         '--lecture', 'TEST',
         '--directory', dir,
         '--authors', 'user1', 'user2',
-        ...configArg
+        ...configArgs
     ], options);
 
     console.log(log.blue('Created config:'));
@@ -63,24 +86,86 @@ const test = (argv) => {
 
     // export assignment - manipulate first to check REMOVE detection
     heading('Export assignment from ./examples');
-    const exampleFile = './examples/baseExample/code/index.ts';
-    const exampleCode = fs.readFileSync(exampleFile, { encoding: 'utf8' });
-    fs.writeFileSync(
-        exampleFile,
-        exampleCode + '// REMOVE BEGIN\ntop secret\n// REMOVE END\n',
-        { encoding: 'utf8' }
-    );
+    const undoEdit = editFile(
+        './examples/baseExample/code/index.ts',
+        (c) => c + '// REMOVE BEGIN\ntop secret\n// REMOVE END\n');
 
     const tempDir = fs.mkdtempSync(os.tmpdir() + path.sep);
-    run(command, [
+    runSync(command, [
         'exportAssignment',
         '--output', tempDir,
         '--config', 'example.json',
         yesArg
     ], options);
 
-    fs.writeFileSync(exampleFile, exampleCode, { encoding: 'utf8' });
+    undoEdit();
 
+    // import assignment
+    heading('Import assignment into ./temp');
+    runSync(command, [
+        'importAssignment',
+        '--input', tempDir,
+        ...configArgs,
+        yesArg
+    ], options);
+
+    // start the server
+    heading('Starting server');
+    const server = run(command, [
+        'start',
+        ...configArgs,
+        '--', '--no-open'
+    ], {
+        stdio: ['inherit', 'pipe', 'inherit'],
+        shell: true,
+        encoding: 'utf8'
+    });
+    const serverRead = new stream.PassThrough();
+    server.stdout.pipe(serverRead);
+    server.stdout.pipe(stdout);
+
+    const regex = /compiled (?:(successfully)|(?:with (\d+) errors)) in/g;
+    const ansi = /\x1b\[.*?m/g;
+    serverRead.on('data', (d) => {
+        const str = d.toString().replace(ansi, '');
+        const match = regex.exec(str);
+        if (!match) return;
+        let status = 'with unknown result';
+        if (!!match[1]) status = 'successfully';
+        if (!!match[2]) status = 'with errors';
+
+        setTimeout(() => {
+            console.log(log.blue(
+                `Started server ${status}. Killing the process...`));
+            kill(server);
+        }, 100);
+    });
+
+    await new Promise((res) => server.on('close', res));
+    console.log(log.blue('Server stopped.'));
+
+    // export assignment - manipulate first to check change detection
+    heading('Export submission from ./temp');
+    editFile(
+        './temp/baseExample/code/index.ts',
+        (c) => '#define true false\n' + c);
+    editFile(
+        './temp/baseExample/exercise.json',
+        (c) => {
+            c.exclude = ['./code/index.ts'];
+            c.include = ['./index.pug'];
+            return c;
+        },
+        true);
+    runSync(command, [
+        'exportSubmission',
+        '--output', tempDir,
+        ...configArgs,
+        yesArg
+    ], options);
+
+    // clean up
+    heading('All done. Cleaning up.');
     fs.rmSync(tempDir, { recursive: true, force: true });
 };
 
