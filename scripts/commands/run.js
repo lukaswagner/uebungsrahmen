d221abf7796c84e699be585a1246a9e6043905b3
Lukas Wagner <lukas@lwgnr.dev>
2021-10-11 15:59:43 +0200

    allow specifying params for scripts

--- scripts/commands/run.js
+++ scripts/commands/run.js
@@ -1,7 +1,10 @@
 'use strict';
 
 exports.command = ['run <command>', 'r'];
-exports.description = 'Runs the given npm script with correct env settings.';
+exports.description =
+    'Runs the given npm script with correct env settings. ' +
+    'If needed, you can pass additional arguments to the script after --. ' +
+    'Example: fw run start -- --no-open';
 exports.handler = require('../handlers/run.js');
 exports.builder = (yargs) => {
     yargs.positional('command', require('../options/command'));
--- scripts/handlers/run.js
+++ scripts/handlers/run.js
@@ -3,7 +3,12 @@
 const child = require('child_process');
 
 const run = (argv) => {
-    child.spawn('npm run ' + argv.command, {
+    const args = ['run', argv.command];
+    const additionalArgs = argv._.slice(1);
+    if (additionalArgs.length > 0) {
+        args.push('--', additionalArgs);
+    }
+    child.spawn('npm', args, {
         stdio: 'inherit',
         shell: true,
         env: Object.assign({ 'fw_config': argv.config }, process.env)
