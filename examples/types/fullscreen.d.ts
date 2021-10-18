2c0431b1ddaa6aa974ce147080c6b959719da735
Lukas Wagner <lukas@lwgnr.dev>
2021-10-18 17:29:59 +0200

    mode fixes

---
+++ examples/types/fullscreen.d.ts
@@ -0,0 +1,6 @@
+import { UI } from '@lukaswagner/web-ui';
+
+export declare function setFullscreen(elem: HTMLElement, full: boolean): void;
+export declare function toggleFullscreen(elem: HTMLElement): void;
+export declare function setupFullscreen(elem: HTMLElement, event: string): void;
+export declare function addFullscreenCheckbox(elem: HTMLElement, ui: UI): void;
